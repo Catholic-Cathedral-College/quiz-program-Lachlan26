281b10fefb8ae8f27f8e8a05c266e63487ddb7dc
LachlanW1 <110640104+Lachlan26@users.noreply.github.com>
2022-09-08 23:48:57 +0000

    I have made my code more efficient

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-
-print (' hello World ')
+#this is the start up to the quiz
+asn1 = input("Do you want to play a game ")
+if asn1 == "no":
+  print ("Too Bad you have to play")
+if asn1 == "yes":
+  print ("Let's get started")
+#This is the start of the questions 
+ans2 =int(input("question 1: What is the best race to make money fast         1       2             3\n                                                      Resort Curcuit, Columbia J.Storm "))
+if ans2 == 1:
+  print ("Correct")
+else:
+  print ("Incorrect")
+asn3 = input("Question 2:What company releised Need For Speed Heat 1          2      3\n                                                 E.A/Origin Bethesda Ubisoft\n")
+if asn3 == "1":
+  print("Correct")
+else: 
+  print ("Incorrect")
+ans4 = (int(input("Question 3:What is the best super car 1            2                 3\n                               Konisegg Regera Lamborghini Aventador Porsche 911\n")))
+if ans4 == 1:
+  print ("Correct")
+else:
+  print("Incorrect")
+ans5 = (int(input("Question 4:When was Need For Speed Heat released 1    2   3\n    2015     2010  1019  ")))
